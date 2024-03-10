7fd4554d80b48878768d647f1859c666574eab5c
Carlos Felgueiras <catofel2000@gmail.com>
2024-03-10 01:33:45 +0000

    feat(server): implemented xu liskov stages in server

--- ServerR2/src/main/java/pt/ulisboa/tecnico/tuplespaces/replicaXuLiskov/server/ServerXuLiskovImpl.java
+++ ServerR2/src/main/java/pt/ulisboa/tecnico/tuplespaces/replicaXuLiskov/server/ServerXuLiskovImpl.java
@@ -8,6 +8,7 @@ import pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.server.domain.ServerState;
 import static io.grpc.Status.CANCELLED;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ServerXuLiskovImpl extends TupleSpacesReplicaImplBase {
 
@@ -34,6 +35,40 @@ public class ServerXuLiskovImpl extends TupleSpacesReplicaImplBase {
             tuple = serverState.read(request.getSearchPattern());
         }
         ReadResponse response = ReadResponse.newBuilder().setResult(tuple).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public synchronized void takePhase1(TakePhase1Request request, StreamObserver<TakePhase1Response> responseObserver) {
+        Optional<List<String>> tuples;
+        
+        while((tuples = serverState.takePhase1(request.getSearchPattern(), request.getClientId())).isEmpty()){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                responseObserver.onError(CANCELLED.withDescription("Take operation interrupted").asRuntimeException());
+            }
+        }
+        
+        TakePhase1Response response = TakePhase1Response.newBuilder().addAllReservedTuples(tuples.get()).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public synchronized void takePhase1Release(TakePhase1ReleaseRequest request, StreamObserver<TakePhase1ReleaseResponse> responseObserver) {
+        serverState.takePhase1Release(request.getClientId());
+        TakePhase1ReleaseResponse response = TakePhase1ReleaseResponse.newBuilder().build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public synchronized void takePhase2(TakePhase2Request request, StreamObserver<TakePhase2Response> responseObserver) {
+        serverState.takePhase2(request.getTuple(), request.getClientId());
+        TakePhase2Response response = TakePhase2Response.newBuilder().build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
--- ServerR2/src/main/java/pt/ulisboa/tecnico/tuplespaces/replicaXuLiskov/server/domain/ServerState.java
+++ ServerR2/src/main/java/pt/ulisboa/tecnico/tuplespaces/replicaXuLiskov/server/domain/ServerState.java
@@ -2,42 +2,110 @@ package pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.server.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ServerState {
 
-  private List<String> tuples;
+  private class Tuple {
+    private String tuple;
+    private boolean locked = false;
+    private int owner;
+
+    public Tuple(String tuple) {
+      this.tuple = tuple;
+    }
+
+    public String getTuple() {
+      return this.tuple;
+    }
+
+    public boolean isLocked() {
+      return this.locked;
+    }
+
+    public int getOwner() {
+      return this.owner;
+    }
+
+    public void lock(int owner) {
+      if (this.locked) {
+        return;
+      }
+      this.locked = true;
+      this.owner = owner;
+    }
+
+    public void unlock(int owner) {
+      if (!this.locked || this.owner != owner) {
+        return;
+      }
+
+      this.locked = false;
+    }
+  }
+
+  private List<Tuple> tuples;
 
   public ServerState() {
-    this.tuples = new ArrayList<String>();
+    this.tuples = new ArrayList<Tuple>();
 
   }
 
   public void put(String tuple) {
-    tuples.add(tuple);
+    tuples.add(new Tuple(tuple));
   }
 
-  private String getMatchingTuple(String pattern) {
-    for (String tuple : this.tuples) {
-      if (tuple.matches(pattern)) {
-        return tuple;
-      }
-    }
-    return null;
+  private Tuple getMatchingTuple(String pattern) {
+    return this.tuples.stream().filter(tuple -> tuple.getTuple().matches(pattern)).findFirst().orElse(null);
   }
 
   public String read(String pattern) {
-    return getMatchingTuple(pattern);
+    Tuple tuple = getMatchingTuple(pattern);
+
+    if (tuple == null) {
+      return null;
+    }
+
+    return tuple.getTuple();
   }
 
-  public String take(String pattern) {
-    String tuple = getMatchingTuple(pattern);
-    if (tuple != null) {
-      this.tuples.remove(tuple);
+  public Optional<List<String>> takePhase1(String pattern, int clientId) {
+    List<String> matchingTuples = new ArrayList<>();
+
+    if (!tuples.stream().filter(tuple -> tuple.getTuple().matches(pattern)).findAny().isPresent()) {
+      return Optional.empty();
     }
-    return tuple;
+
+    tuples.stream()
+        .filter(tuple -> !tuple.isLocked() && tuple.getTuple().matches(pattern))
+        .forEach(tuple -> {
+          tuple.lock(clientId);
+          matchingTuples.add(tuple.getTuple());
+        });
+
+    return Optional.of(matchingTuples);
+  }
+
+  public void takePhase1Release(int clientId) {
+    tuples.stream()
+        .filter(tuple -> tuple.isLocked() && tuple.getOwner() == clientId)
+        .forEach(tuple -> tuple.unlock(clientId));
+  }
+
+  public void takePhase2(String tuple, int clientId) {
+    Tuple matchingTuple = tuples.stream()
+        .filter(t -> t.getTuple().equals(tuple) && t.isLocked() && t.getOwner() == clientId)
+        .findFirst()
+        .orElseThrow(() -> new RuntimeException("No matching tuple found"));
+
+    tuples.remove(matchingTuple);
   }
 
   public List<String> getTupleSpacesState() {
-    return this.tuples;
+    List<String> resultingTuples = new ArrayList<>();
+
+    tuples.stream().forEach(tuple -> resultingTuples.add(tuple.getTuple()));
+
+    return resultingTuples;
   }
 }
