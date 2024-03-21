9bbdf4a4970de07c0c5a5e54f70b3f45ccdd3e61
Carlos Felgueiras <catofel2000@gmail.com>
2024-03-21 17:22:08 +0000

    feat: implemented defered takes structure, and put already notifies the defered take

--- ServerR3/src/main/java/pt/ulisboa/tecnico/tuplespaces/replicaTotalOrder/ServerTotalOrderImpl.java
+++ ServerR3/src/main/java/pt/ulisboa/tecnico/tuplespaces/replicaTotalOrder/ServerTotalOrderImpl.java
@@ -4,6 +4,7 @@ import io.grpc.stub.StreamObserver;
 import static io.grpc.Status.CANCELLED;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.concurrent.locks.*;
 import pt.ulisboa.tecnico.tuplespaces.replicaTotalOrder.contract.TupleSpacesReplicaGrpc.TupleSpacesReplicaImplBase;
 import pt.ulisboa.tecnico.tuplespaces.replicaTotalOrder.contract.TupleSpacesReplicaTotalOrder.*;
@@ -12,45 +13,108 @@ import pt.ulisboa.tecnico.tuplespaces.replicaTotalOrder.server.domain.ServerStat
 public class ServerTotalOrderImpl extends TupleSpacesReplicaImplBase {
 
     private Lock lock = new ReentrantLock();
-    private Condition condition = lock.newCondition();
     private ServerState serverState = new ServerState();
+    private List<DeferedTake> deferedTakes = new ArrayList<>();
+    private Condition totalOrderCondition = lock.newCondition();
+    private int currSeq = 0;
+
+    private class DeferedTake {
+        private String pattern;
+        private Lock lock;
+        private Condition condition;
+        private String tuple;
+
+        public DeferedTake(String pattern, Lock lock) {
+            this.pattern = pattern;
+            this.lock = lock;
+            this.condition = lock.newCondition();
+        }
+
+        // Should be called in lock's mutual exclusion region
+        public String awaitTuple() {
+            while(tuple == null) {
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    return null;
+                }
+            }
+            return tuple;
+        }
+
+        // Should be called in lock's mutual exclusion region
+        public void giveTuple(String tuple) {
+            this.tuple = tuple;
+            condition.signalAll();
+        }
+
+        public boolean matches(String tuple) {
+            return tuple.matches(pattern);
+        }
+    }
 
     @Override
     public void put(PutRequest request, StreamObserver<PutResponse> responseObserver) {
         lock.lock();
+        awaitTurn(request.getSeqNumber());
+
+        for (DeferedTake deferedTake : deferedTakes) {
+            if (deferedTake.matches(request.getNewTuple())) {
+                deferedTake.giveTuple(request.getNewTuple());
+                deferedTakes.remove(deferedTake);
+
+                responseObserver.onNext(PutResponse.newBuilder().build());
+                responseObserver.onCompleted();
+
+                nextTurn();
+                lock.unlock();
+                return;
+            }
+        }
+
         serverState.put(request.getNewTuple());
-        condition.signalAll();
+        totalOrderCondition.signalAll();
+
         PutResponse response = PutResponse.newBuilder().build();
+
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+
+        nextTurn();
         lock.unlock();
     }
 
     @Override
     public void read(ReadRequest request, StreamObserver<ReadResponse> responseObserver) {
         lock.lock();
+
         String tuple = serverState.read(request.getSearchPattern());
+
         while(tuple == null){
             try {
-                condition.await();
+                totalOrderCondition.await();
             } catch (InterruptedException e) {
                 responseObserver.onError(CANCELLED.withDescription("Read operation interrupted").asRuntimeException());
             }
             tuple = serverState.read(request.getSearchPattern());
         }
+
         ReadResponse response = ReadResponse.newBuilder().setResult(tuple).build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+
         lock.unlock();
     }
 
     @Override
     public void take(TakeRequest request, StreamObserver<TakeResponse> responseObserver) {
         lock.lock();
+
+        // TODO: implement deferd takes
         String tuple = serverState.take(request.getSearchPattern());
         while(tuple == null){
             try {
-                condition.await();
+                totalOrderCondition.await();
             } catch (InterruptedException e) {
                 responseObserver.onError(CANCELLED.withDescription("Take operation interrupted").asRuntimeException());
             }
@@ -59,6 +123,7 @@ public class ServerTotalOrderImpl extends TupleSpacesReplicaImplBase {
         TakeResponse response = TakeResponse.newBuilder().setResult(tuple).build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+
         lock.unlock();
     }
 
@@ -70,6 +135,23 @@ public class ServerTotalOrderImpl extends TupleSpacesReplicaImplBase {
         responseObserver.onNext(response);
         responseObserver.onCompleted();
         lock.unlock();
+    }
+
+    // Should Be executed in lock's mutual exclusion region
+    private void awaitTurn(int seqNumber) {
+        while (currSeq != seqNumber) {
+            try {
+                totalOrderCondition.await();
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    }
+
+    // Should Be executed in lock's mutual exclusion region
+    private void nextTurn() {
+        currSeq++;
+        totalOrderCondition.signalAll();
     }
 
 }
